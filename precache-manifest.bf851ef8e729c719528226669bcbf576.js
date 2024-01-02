e0dc86052d253dbf3a2cce54a09fbd404334115d
Teo <teodora.botezatu@epitech.eu>
2024-01-02 15:16:51 +0100

    Updates

---
+++ precache-manifest.bf851ef8e729c719528226669bcbf576.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b888e84f8916cdece6ead6c6d974c0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b9d94b8a4e9210e4bc5",
+    "url": "/static/css/main.1fe1a910.chunk.css"
+  },
+  {
+    "revision": "7bcbcfcfcb53809c1989",
+    "url": "/static/js/2.dbb27596.chunk.js"
+  },
+  {
+    "revision": "7b9d94b8a4e9210e4bc5",
+    "url": "/static/js/main.f2e64ba0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "73409420f4053dd57a2d6f3e28d3236c",
+    "url": "/static/media/api-photo.73409420.png"
+  },
+  {
+    "revision": "bf897da96ddb969f99aea67f12c932e9",
+    "url": "/static/media/profile-no-back.bf897da9.png"
+  },
+  {
+    "revision": "aede34bb7e7d7f519e452a470650e351",
+    "url": "/static/media/project-photo.aede34bb.png"
+  }
+]);
